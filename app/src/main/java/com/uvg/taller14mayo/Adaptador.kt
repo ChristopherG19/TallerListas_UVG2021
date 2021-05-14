ab7ee3ce2a8076f21923ec13b3a01735087b0590
ChristopherG19 <60325784+ChristopherG19@users.noreply.github.com>
2021-05-14 14:17:13 -0600

    Primer Commit

---
+++ app/src/main/java/com/uvg/taller14mayo/Adaptador.kt
@@ -0,0 +1,21 @@
+package com.uvg.taller14mayo
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class Adaptador: RecyclerView.Adapter<Adaptador.ItemsViewHolder>(){
+    class ItemsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemsViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ItemsViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
