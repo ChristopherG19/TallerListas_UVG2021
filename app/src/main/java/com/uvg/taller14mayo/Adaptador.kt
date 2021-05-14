c963e9019077c0c2e1cd972ba61ac9d24b4a260e
ChristopherG19 <60325784+ChristopherG19@users.noreply.github.com>
2021-05-14 15:21:30 -0600

    Se agregan elementos a las listas y se modifican algunos métodos

--- app/src/main/java/com/uvg/taller14mayo/Adaptador.kt
+++ app/src/main/java/com/uvg/taller14mayo/Adaptador.kt
@@ -3,12 +3,13 @@ package com.uvg.taller14mayo
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ExpandableListView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_lista.view.*
 
-class Adaptador(var items: MutableList<String>): RecyclerView.Adapter<Adaptador.ItemsViewHolder>(){
+class Adaptador(private val clickListener: (Int) -> Unit): RecyclerView.Adapter<Adaptador.ItemsViewHolder>(){
 
-    //private var items: MutableList<String> = mutableListOf()
+    private var items: MutableList<String> = mutableListOf()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemsViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_lista, parent, false)
@@ -17,16 +18,27 @@ class Adaptador(var items: MutableList<String>): RecyclerView.Adapter<Adaptador.
 
     override fun onBindViewHolder(holder: ItemsViewHolder, position: Int) {
         val item = items[position]
-        holder.bind(item)
+        holder.bind(item,clickListener)
     }
 
     override fun getItemCount(): Int {
         return items.size
     }
 
+    fun setItems(nuevosItems: MutableList<String>){
+        this.items = nuevosItems
+        notifyDataSetChanged()
+    }
+
+    fun getItem(position: Int) : String{
+        return items[position]
+    }
+
     class ItemsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        fun bind(item:String) = with(itemView){
+        fun bind(item:String, listener: (Int) -> Unit) = with(itemView){
             txtTitulo.text = item
+
+            setOnClickListener { listener(adapterPosition) }
         }
     }
 }
--- app/src/main/java/com/uvg/taller14mayo/MainActivity.kt
+++ app/src/main/java/com/uvg/taller14mayo/MainActivity.kt
@@ -2,25 +2,58 @@ package com.uvg.taller14mayo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.FieldPosition
 
 class MainActivity : AppCompatActivity() {
 
-    //private val adaptador = Adaptador()
+    private val adaptador = Adaptador({
+            clickListener -> showItemClick(clickListener)
+    })
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*val items : MutableList<String> = mutableListOf()
+        items.add("Prueba 1")
+        items.add("Prueba 2")
+        items.add("Prueba 3")
+        items.add("Prueba 4")
+        items.add("Prueba 5")
+        items.add("Prueba 6")
+        items.add("Prueba 7")
+        items.add("Prueba 8")
+        items.add("Prueba 9")
+        items.add("Prueba 10")
+        items.add("Prueba 11")
+        items.add("Prueba 12")
+        items.add("Prueba 13")
+        items.add("Prueba 14")
+        items.add("Prueba 15")
+        items.add("Prueba 16")
+        items.add("Prueba 17")
+        items.add("Prueba 18")
+        items.add("Prueba 19")
+        items.add("Prueba 20")*/
+
+        //val adaptador = Adaptador(items)
 
         val items : MutableList<String> = mutableListOf()
         items.add("Prueba 1")
         items.add("Prueba 2")
         items.add("Prueba 3")
 
-        val adaptador = Adaptador(items)
+        adaptador.setItems(items)
 
         lista.layoutManager = LinearLayoutManager(this)
         lista.adapter = adaptador
+    }
+
+    fun showItemClick(position: Int) {
+        val item = adaptador.getItem(position)
+        Toast.makeText(this, item, Toast.LENGTH_SHORT).show()
     }
 }
