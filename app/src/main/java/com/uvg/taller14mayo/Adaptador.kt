30dd61574ab1ff231b45e1126c692ce6d00f374e
ChristopherG19 <60325784+ChristopherG19@users.noreply.github.com>
2021-05-28 17:12:23 -0600

    Arreglos menores

--- app/src/main/java/com/uvg/taller14mayo/Adaptador.kt
+++ app/src/main/java/com/uvg/taller14mayo/Adaptador.kt
@@ -8,7 +8,7 @@ import android.widget.ExpandableListView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_lista.view.*
 
-class Adaptador(private val clickListener: (Int) -> Unit, private val LongClickListener: (Int) -> Unit): RecyclerView.Adapter<Adaptador.ItemsViewHolder>(){
+class Adaptador(private val clickListener: (Int) -> Unit, private val longClickListener: (Int)->Unit ): RecyclerView.Adapter<Adaptador.ItemsViewHolder>() {
 
     private var items: MutableList<String> = mutableListOf()
 
@@ -19,32 +19,37 @@ class Adaptador(private val clickListener: (Int) -> Unit, private val LongClickL
 
     override fun onBindViewHolder(holder: ItemsViewHolder, position: Int) {
         val item = items[position]
-        holder.bind(item,clickListener,LongClickListener)
+        holder.bind(item, clickListener, longClickListener)
     }
 
     override fun getItemCount(): Int {
         return items.size
     }
 
-    fun setItems(nuevosItems: MutableList<String>){
+    fun setItems(nuevosItems: MutableList<String>) {
         this.items = nuevosItems
         notifyDataSetChanged()
     }
 
-    fun getItem(position: Int) : String{
+    fun getItem(position: Int): String {
         return items[position]
     }
 
-    fun RemoveItem(position: Int){
+    fun RemoveItem(position: Int) {
         items.removeAt(position)
         notifyDataSetChanged()
     }
 
-    class ItemsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        fun bind(item:String, listener: (Int) -> Unit, LongClickListener: (Int) -> Unit) = with(itemView){
+    fun ChangeItem(position: Int) {
+        items[position] = "Modificado"
+        notifyDataSetChanged()
+    }
+
+    class ItemsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(item: String, listener: (Int) -> Unit, longListener: (Int) -> Unit) = with(itemView) {
             txtTitulo.text = item
             setOnClickListener { listener(adapterPosition) }
-            setOnLongClickListener { LongClickListener(adapterPosition) }
+            setOnLongClickListener { longListener(longListener) }
         }
     }
 }
--- app/src/main/java/com/uvg/taller14mayo/MainActivity.kt
+++ app/src/main/java/com/uvg/taller14mayo/MainActivity.kt
@@ -2,25 +2,32 @@ package com.uvg.taller14mayo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 import java.text.FieldPosition
 
+
 class MainActivity : AppCompatActivity() {
 
-    private val adaptador = Adaptador({
-            clickListener -> showItemClick(clickListener)
-    },{
-        longClickListener -> showItemClick(longClickListener)
-    })
+    private val adaptador = Adaptador(
+        { clickListener ->
+            showItemClick(clickListener)
+        },
+        {
+            longClickListener ->
+            showItemClick(longClickListener)
+        }
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //val adaptador = Adaptador(items)
 
-        val items : MutableList<String> = mutableListOf()
+        val items: MutableList<String> = mutableListOf()
         items.add("Elemento (1)")
         items.add("Elemento (2)")
         items.add("Elemento (3)")
@@ -32,11 +39,12 @@ class MainActivity : AppCompatActivity() {
 
         var i: Int = 4
 
-        btnAdd.setOnClickListener{
+        btnAdd.setOnClickListener {
             items.add("Elemento ($i)")
             adaptador.setItems(items)
             i++
         }
+
     }
 
     fun showItemClick(position: Int) {
